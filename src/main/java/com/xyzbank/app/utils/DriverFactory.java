0ba9dd5ee36454f2574e95add33e3d2dc93b48d1
Kwame Oduro <odurokwameee@gmail.com>
2025-07-04 15:16:29 +0000

    utils: update driverfactory for robust webdriver management

--- src/main/java/com/xyzbank/app/utils/DriverFactory.java
+++ src/main/java/com/xyzbank/app/utils/DriverFactory.java
@@ -6,21 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-// Note: No 'import io.github.bonigarcia.wdm.WebDriverManager;' needed here
-// because we will use its fully qualified name directly to avoid conflict.
 
-public class DriverFactory { // Renamed from WebDriverManager
+public class DriverFactory {
 
-    /**
-     * Initializes and returns a new WebDriver instance based on the provided browser name.
-     * This method is designed to be called once per test setup to get a fresh driver.
-     *
-     * @param browserName The name of the browser (e.g., "chrome", "firefox", "edge", "safari").
-     * @return A new WebDriver instance.
-     * @throws IllegalArgumentException if an unsupported browser name is provided.
-     */
     public static WebDriver getNewDriver(String browserName) {
-        WebDriver driver; // Local variable, not static
+        WebDriver driver;
 
         switch (browserName.toLowerCase()) {
             case "chrome":
