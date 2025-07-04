3502c6e6303451b89b3efd331e604dd8db7dd8ce
Kwame Oduro <odurokwameee@gmail.com>
2025-07-04 16:29:08 +0000

    utils: update driverfactory for robust webdriver management

--- src/main/java/com/xyzbank/app/utils/DriverFactory.java
+++ src/main/java/com/xyzbank/app/utils/DriverFactory.java
@@ -1,10 +1,14 @@
 package com.xyzbank.app.utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 
 public class DriverFactory {
@@ -16,17 +20,24 @@ public class DriverFactory {
             case "chrome":
                 io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup(); // Use fully qualified name
                 driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new"); // For newer Chrome versions
+                chromeOptions.addArguments("--no-sandbox");
+
                 break;
             case "firefox":
                 io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver().setup(); // Use fully qualified name
                 driver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions  = new FirefoxOptions ();
+                firefoxOptions .addArguments("--headless=new"); // For newer Chrome versions
+                firefoxOptions .addArguments("--no-sandbox");
                 break;
             case "edge":
                 io.github.bonigarcia.wdm.WebDriverManager.edgedriver().setup(); // Use fully qualified name
                 driver = new EdgeDriver();
-                break;
-            case "safari":
-                driver = new SafariDriver();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new"); // For newer Chrome versions
+                edgeOptions.addArguments("--no-sandbox");
                 break;
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browserName);
